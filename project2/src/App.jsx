8127bacbe0e00b69a6c74c65cd906a9def7020c0
raccoonjeong <rana3782@gmail.com>
2025-09-18 19:32:40 +0900

    useReducer still have error

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 import TestComp from "./components/TestComp";
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 
 const mockTodos = [
   {
@@ -30,41 +30,71 @@ const mockTodos = [
     createdDate: new Date().getTime(),
   },
 ];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, isDone: !it.isDone } : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
 function App() {
-  const [todos, setTodos] = useState(mockTodos);
+  // const [todos, setTodos] = useState(mockTodos);
   const idRef = useRef(4);
+  const [todos, dispatch] = useReducer(useReducer, mockTodos);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current,
-      isDone: false,
-      content,
-      createDate: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        isDone: false,
+        content,
+        createDate: new Date().getTime(),
+      },
+    });
+    // setTodos([newTodo, ...todos]);
     idRef.current += 1;
   };
 
   const onUpdate = (targetId) => {
     console.log("targetId", targetId);
-    setTodos(
-      todos.map((it) => {
-        return it.id === targetId
-          ? {
-              ...it,
-              isDone: !it.isDone,
-            }
-          : it;
-      })
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+    // setTodos(
+    //   todos.map((it) => {
+    //     return it.id === targetId
+    //       ? {
+    //           ...it,
+    //           isDone: !it.isDone,
+    //         }
+    //       : it;
+    //   })
+    // );
   };
   const onDelete = (targetId) => {
-    setTodos(todos.filter((it) => it.id !== targetId));
+    // setTodos(todos.filter((it) => it.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return (
     <div className="App">
-      <TestComp />
+      {/* <TestComp /> */}
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
